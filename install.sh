24fa724f8a96d823fa04f24d83793dc8761d013b
Anna <anapple07@users.noreply.github.com>
2019-02-02 15:28:55 +0900

    update insall.sh

--- install.sh
+++ install.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
 
+if [ ! -e ~/.vim/dein/repos/github.com/Shougo/dein.vim ]; then
 mkdir -p ~/.vim/dein/repos/github.com/Shougo/dein.vim
 git clone https://github.com/Shougo/dein.vim.git \
     ~/.vim/dein/repos/github.com/Shougo/dein.vim
-
+fi
 
 wget https://raw.github.com/git/git/master/contrib/completion/git-completion.bash
 wget https://raw.github.com/git/git/master/contrib/completion/git-prompt.sh
 
-ln -s $HOME/dotfiles/.vimrc ~/.vimrc
-ln -s $HOME/dotfiles/.bashrc ~/.bashrc
+ln -fs $HOME/dotfiles/.vimrc ~/.vimrc
+ln -fs $HOME/dotfiles/.bashrc ~/.bashrc
 source ~/.bashrc
 
 echo 'Setup End'
